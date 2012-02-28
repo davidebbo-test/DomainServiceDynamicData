14bac96529cedb631ff3615b93a23b40c725804d
davidebbo <david.ebbo@microsoft.com>
2012-02-28 11:40:18 -0800

    Add Domain Service

---
+++ DomainServiceDynamicData/Northwind.cs
@@ -0,0 +1,104 @@
+﻿
+namespace DomainServiceDynamicData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // Implements application logic using the NorthwindEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    // TODO: add the EnableClientAccessAttribute to this class to expose this DomainService to clients.
+    public class Northwind : LinqToEntitiesDomainService<NorthwindEntities>
+    {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Categories' query.
+        public IQueryable<Category> GetCategories()
+        {
+            return this.ObjectContext.Categories.OrderBy(c => c.CategoryID);
+        }
+
+        public void InsertCategory(Category category)
+        {
+            if ((category.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(category, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Categories.AddObject(category);
+            }
+        }
+
+        public void UpdateCategory(Category currentCategory)
+        {
+            this.ObjectContext.Categories.AttachAsModified(currentCategory, this.ChangeSet.GetOriginal(currentCategory));
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            if ((category.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(category, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Categories.Attach(category);
+                this.ObjectContext.Categories.DeleteObject(category);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Products' query.
+        public IQueryable<Product> GetProducts()
+        {
+            return this.ObjectContext.Products.OrderBy(p => p.ProductID);
+        }
+
+        public void InsertProduct(Product product)
+        {
+            if ((product.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(product, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Products.AddObject(product);
+            }
+        }
+
+        public void UpdateProduct(Product currentProduct)
+        {
+            this.ObjectContext.Products.AttachAsModified(currentProduct, this.ChangeSet.GetOriginal(currentProduct));
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            if ((product.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(product, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Products.Attach(product);
+                this.ObjectContext.Products.DeleteObject(product);
+            }
+        }
+    }
+}
+
+
---
+++ DomainServiceDynamicData/Northwind.metadata.cs
@@ -0,0 +1,99 @@
+﻿
+namespace DomainServiceDynamicData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Objects.DataClasses;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // The MetadataTypeAttribute identifies CategoryMetadata as the class
+    // that carries additional metadata for the Category class.
+    [MetadataTypeAttribute(typeof(Category.CategoryMetadata))]
+    public partial class Category
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the Category class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class CategoryMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private CategoryMetadata()
+            {
+            }
+
+            public int CategoryID { get; set; }
+
+            public string CategoryName { get; set; }
+
+            public string Description { get; set; }
+
+            public byte[] Picture { get; set; }
+
+            public EntityCollection<Product> Products { get; set; }
+        }
+    }
+
+    // The MetadataTypeAttribute identifies ProductMetadata as the class
+    // that carries additional metadata for the Product class.
+    [MetadataTypeAttribute(typeof(Product.ProductMetadata))]
+    public partial class Product
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the Product class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class ProductMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private ProductMetadata()
+            {
+            }
+
+            public Category Category { get; set; }
+
+            public Nullable<int> CategoryID { get; set; }
+
+            public bool Discontinued { get; set; }
+
+            public EntityCollection<Order_Detail> Order_Details { get; set; }
+
+            public int ProductID { get; set; }
+
+            public string ProductName { get; set; }
+
+            public string QuantityPerUnit { get; set; }
+
+            public Nullable<short> ReorderLevel { get; set; }
+
+            public Supplier Supplier { get; set; }
+
+            public Nullable<int> SupplierID { get; set; }
+
+            public Nullable<decimal> UnitPrice { get; set; }
+
+            public Nullable<short> UnitsInStock { get; set; }
+
+            public Nullable<short> UnitsOnOrder { get; set; }
+        }
+    }
+}
